8304b665d1d520e9f8207066ad7fbb6e4a3b5c8d
ashish <ashishbadgujar124@gmail.com>
2021-06-22 09:59:41 +0530

    port line changed

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on('connection', socket => {
     });
 })
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
