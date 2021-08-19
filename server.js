51b4e48a4fdceb3cba69b65567f64ef2d88d5c21
Ashish <ashish@reapit.in>
2021-08-19 10:29:42 +0530

    port change

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ io.on('connection', socket => {
     });
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
